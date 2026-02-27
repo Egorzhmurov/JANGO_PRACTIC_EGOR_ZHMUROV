3e72b1aca4a38f6be8019486f4af51715a745aef
Egorzhmurov <zhmurovegor99@gmail.com>
2026-02-27 16:14:30 +0200

    Connected db

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Таблиця для Питань
+class Question(models.Model):
+    question_text = models.CharField(max_length=200) # Текст питання
+    pub_date = models.DateTimeField("date published") # Дата публікації
+
+# Таблиця для Варіантів відповідей
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) # Прив'язка до питання
+    choice_text = models.CharField(max_length=200) # Текст відповіді
+    votes = models.IntegerField(default=0) # Кількість голосів (спочатку 0
