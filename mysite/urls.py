7ea052b4a3ec2cbd100dcfe7fb0f505d9762f4bc
Egorzhmurov <zhmurovegor99@gmail.com>
2026-02-28 17:26:41 +0200

    Created Admin panel

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('polls.urls')),  # <-- Убрали слово polls/ вот тут
-    path('admin/', admin.site.urls),
+    # Теперь админка будет открываться сразу на главной странице (127.0.0.1:8000/)
+    path('', admin.site.urls),
+
+    # А приложение с опросами будет доступно по адресу 127.0.0.1:8000/polls/
+    path('polls/', include('polls.urls')),
 ]
--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 
-# Таблиця для Питань
 class Question(models.Model):
-    question_text = models.CharField(max_length=200) # Текст питання
-    pub_date = models.DateTimeField("date published") # Дата публікації
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
 
-# Таблиця для Варіантів відповідей
+    def __str__(self):
+        return self.question_text
+
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE) # Прив'язка до питання
-    choice_text = models.CharField(max_length=200) # Текст відповіді
-    votes = models.IntegerField(default=0) # Кількість голосів (спочатку 0
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
